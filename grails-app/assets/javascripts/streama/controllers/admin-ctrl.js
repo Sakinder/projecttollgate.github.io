be08612120307bd02820ab59fe3334e8992537c6
projecttollgate <projecttollgate@gmail.com>
2017-08-26 01:19:09 -0500

    rev3
    rev3

--- grails-app/assets/javascripts/streama/controllers/admin-ctrl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('streama').controller('adminCtrl', ['$scope', 'apiService', 'modalService', '$rootScope', function ($scope, apiService, modalService, $rootScope) {
-
-}]);
-
-
-
-
